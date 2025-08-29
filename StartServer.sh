dbee84e8d935ee80e6843105dbf6965418862212
Brandon Pike <160025656+GearedMountain@users.noreply.github.com>
2025-08-29 19:51:53 -0400

    server creator

---
+++ StartServer.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+echo "This product requires python 3 to be pre-installed in the environment"
+yes | sudo apt install python3-venv > /dev/null 2>&1
+
+# Optional: create virtual environment if it doesn't exist
+if [ ! -d "venv" ]; then
+    echo "Creating virtual environment..."
+    python3 -m venv venv
+fi
+
+
+# Activate virtual environment
+echo "Enabling virtual environment..."
+source ./venv/bin/activate
+
+# Upgrade pip
+echo "Ensuring pip status..."
+yes | sudo apt install python3-pip > /dev/null 2>&1
+pip install --upgrade pip > /dev/null 2>&1
+
+# Install dependencies
+if [ -f requirements.txt ]; then
+    echo "Installing dependencies..."
+    pip install -r requirements.txt > /dev/null 2>&1
+else
+    echo "Error: requirements.txt not found!"
+    exit 1
+fi
+
+# Optional: check for environment variables
+echo "Searching for environment variables..."
+if [ -f .env ]; then
+    export $(grep -v '^#' .env | xargs)
+fi
+
+if [ -z "$SECRET_KEY" ]; then
+    echo "Warning: FLASK_SECRET_KEY not set. Using default (not secure for production)."
+    export SECRET_KEY="dev"
+fi
+APPNAME="PikeNet"
+if [ ! -d "postgresql-16.2" ]; then
+
+echo "Preparing postgres..."
+mkdir -p $HOME/$APPNAME/pgsql
+if [ ! -f postgresql-16.2.tar.gz ]; then
+echo "Downloading postgres..."
+curl -O https://ftp.postgresql.org/pub/source/v16.2/postgresql-16.2.tar.gz
+else
+echo "Postgresq already downloaded"
+fi
+tar -xf postgresql-16.2.tar.gz
+
+# Readline is required for any psql interaction ( terminal database editing )
+yes | sudo apt install libreadline-dev
+echo "Configuring postgres..."
+cd postgresql-16.2
+./configure --without-icu -prefix=$HOME/$APPNAME/pgsql > /dev/null 2>&1 # No icu makes it hard for postgres to handle foreign characters, ensure english is the only required language
+echo "Building postgres..."
+make -j$(nproc) > /dev/null 2>&1
+
+make install > /dev/null 2>&1
+cd ..
+else
+echo "Postgres already installed"
+fi
+if [ ! -d "~/$APPNAME/local/data" ]; then
+echo "Initializing first database..."
+~/$APPNAME/pgsql/bin/initdb -D ~/$APPNAME/local/data
+~/$APPNAME/pgsql/bin/pg_ctl -D ~/$APPNAME/local/data -l ~/$APPNAME/logfile start -o "-p 5433"
+
+~/$APPNAME/pgsql/bin/createuser -p 5433 pikenet-database-owner
+~/$APPNAME/pgsql/bin/createdb -p 5433 pikenet-database -O pikenet-database-owner
+
+#IDK IF THIS WORKS
+~/$APPNAME/pgsql/bin/psql -p 5433 -d pikenet-database -c "ALTER USER \"pikenet-database-owner\" WITH PASSWORD 'your_password_here';"
+else
+echo "Database already created, skipping step..."
+fi
+# Run Flask app
+echo "Starting web application..."
+python3 PikeNet.py
