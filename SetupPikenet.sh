e8712d7d7b0c3583665ccc75d014df27fc9a603c
gearedMountainVM1 <gearedmountain@gmail.com>
2025-09-16 14:09:25 -0400

    new field

--- SetupPikenet.sh
+++ SetupPikenet.sh
@@ -82,7 +82,8 @@ CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
     username VARCHAR(100) UNIQUE NOT NULL,
     email VARCHAR(120) UNIQUE NOT NULL,
-    password VARCHAR(200) NOT NULL
+    password VARCHAR(200) NOT NULL,
+    authorization INTEGER NOT NULL DEFAULT 2
 );
 
 -- Create the notes table
