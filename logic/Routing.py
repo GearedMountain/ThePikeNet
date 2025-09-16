1ed31b79ca27c2b9abd41326632e0deb323425ab
gearedMountainVM1 <gearedmountain@gmail.com>
2025-09-16 16:09:58 -0400

    i need a utilities file fr

--- logic/Routing.py
+++ logic/Routing.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, jsonify
+from flask import Blueprint, render_template, request, jsonify, redirect
 from logic.Authenticator import *
 from .IntelStackDB import *
 
