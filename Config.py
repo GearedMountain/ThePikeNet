d7c8e02048a77be143417c26b554619d32fb908f
gearedMountainVM1 <gearedmountain@gmail.com>
2025-09-17 16:47:23 -0400

    local database uri

--- Config.py
+++ Config.py
@@ -1,7 +1,7 @@
 class Config:
     DEBUG = False
     SECRET_KEY = 'your-secret-key'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://pikenet-database-owner@localhost:5433/pikenet-database'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class DevelopmentConfig(Config):
