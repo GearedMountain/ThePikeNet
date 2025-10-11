52e560c89f215eb958f753a5d03b50d8799da588
Brandon Pike <160025656+GearedMountain@users.noreply.github.com>
2025-10-11 11:36:02 -0400

    abspath

--- pikenet/webapps/snackbox/routes.py
+++ pikenet/webapps/snackbox/routes.py
@@ -2,6 +2,7 @@ from flask import render_template, request, redirect, url_for, session, jsonify,
 from pikenet.utils.decorators import login_required, role_required
 from .snackboxAPI import runSnackboxAPI
 from . import bp
+import os
 
 @bp.route('/snackbox-api')
 @role_required(0)
@@ -14,4 +15,5 @@ def index():
 @bp.route('/image/<filename>')
 @role_required(0)
 def get_image(filename):
-    return send_from_directory('dynamic/scandinavia', filename)
+    currentCountryFoler = os.path.abspath('dynamic/mexico')
+    return send_from_directory(currentCountryFoler, filename)
