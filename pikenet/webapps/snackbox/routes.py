1d749f6c07a1d6719ba6a383dbcd38b699c5c5d6
Brandon Pike <160025656+GearedMountain@users.noreply.github.com>
2025-10-12 11:52:42 -0400

    fetqch files

--- pikenet/webapps/snackbox/routes.py
+++ pikenet/webapps/snackbox/routes.py
@@ -13,36 +13,34 @@ def index():
     return "running"
 
 
-@bp.route('/snackbox/image/<countryname>/', defaults={'filename': None})
-@bp.route('/snackbox/image/<countryname>/<filename>')
+#@bp.route('/snackbox/image/<countryname>/', defaults={'filename': None})
+@bp.route('/snackbox/image/<filename>')
 @role_required(2, 1, 0)
-def get_image_or_list(countryname, filename):
+def getSnackboxImage(filename):
     # Secure the country name to avoid path traversal
-    safe_countryname = secure_filename(countryname)
-    base_path = 'pikenet/webapps/snackbox/dynamic'
-    country_folder = os.path.abspath(os.path.join(base_path, safe_countryname))
+    safeFilename = secure_filename(filename)
+    basePath = 'pikenet/webapps/snackbox/dynamic'
+    countryFolder = os.path.abspath(os.path.join(basePath, safeFilename))
 
-    if not os.path.exists(country_folder):
+    if not os.path.exists(countryFolder):
         print("country not found")
-
-    if filename is None:
-        # Return a list of files in the folder
-        try:
-            files = os.listdir(country_folder)
-            # Filter out hidden files or directories
-            visible_files = [f for f in files if not f.startswith('.') and os.path.isfile(os.path.join(country_folder, f))]
-            return jsonify(visible_files)
-        except Exception as e:
-            print(f"error returning files: {e}")
     else:
-        # Secure the filename too
-        safe_filename = secure_filename(filename)
-        file_path = os.path.join(country_folder, safe_filename)
+        file_path = os.path.join(countryFolder, safeFilename)
 
         if not os.path.isfile(file_path):
            print("file not found")
-        return send_from_directory(country_folder, safe_filename)
+        return send_from_directory(countryFolder, safeFilename)
     
-@bp.route('/snackbox/image/current')
+@bp.route('/snackbox/image-list')
 def getCurrentCountryName():
-    return getCurrentCountry()
+    currentCountry = getCurrentCountry()
+    basePath = 'pikenet/webapps/snackbox/dynamic'
+    countryFolder = os.path.abspath(os.path.join(basePath, currentCountry))
+    try:
+            files = os.listdir(countryFolder)
+            # Filter out hidden files or directories
+            visible_files = [f for f in files if not f.startswith('.') and os.path.isfile(os.path.join(countryFolder, f))]
+            return jsonify(visible_files)
+    except Exception as e:
+            print(f"error returning files: {e}")
+    return  "Error"
