23b6ffbc5caa36945b2013782b60c35b769415bc
Brandon Pike <160025656+GearedMountain@users.noreply.github.com>
2025-11-23 09:07:08 -0500

    idk

--- pikenet/webapps/main/routes.py
+++ pikenet/webapps/main/routes.py
@@ -11,7 +11,7 @@ def index():
     if session.get("auth_value") is None:
         return redirect(url_for("main.login"))
     else:
-        return redirect(url_for("snackbox"))
+        return redirect(url_for("snackbox.index"))
     print(session.get("auth_value"))
     return render_template(
         "index.html",
